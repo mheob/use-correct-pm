45dd0c8450031a854fc5a8cc698c2c3dc55bc2da
Alex Böhm <tools@boehm.work>
2022-11-14 18:56:09 +0100

    perf: reduce the package size

--- cmd/use_correct_pm.go
+++ cmd/use_correct_pm.go
@@ -13,7 +13,7 @@ import (
 
 var rootCmd = &cobra.Command{
 	Use:     "use-correct-pm",
-	Version: "1.0.3",
+	Version: "1.0.4",
 	Short:   "A simple check of the usage of the correct package manager",
 	Long: `Check if the correct package manager is used.
 Available are 'NPM', 'Yarn' and 'PNPM'.`,
